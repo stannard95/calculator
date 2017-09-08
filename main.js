94c4e28d27ee5e1faef49bb084b8048da37feb58
stannard95 <14066588@brookes.ac.uk>
2017-09-08 22:13:15 +0100

    created index.html and main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+var choice = prompt('What operator would you like to do?');
+
+var firstNumber = prompt('What is your first number?');
+firstNumber = parseFloat(firstNumber);
+var secondNumber = prompt('What is your second number?');
+secondNumber = parseFloat(secondNumber);
+
+if (choice === '+') {
+	alert(firstNumber + secondNumber);
+} else if (choice === '-') {
+	alert(firstNumber - secondNumber);
+}
+else if (choice === '*') {
+	alert(firstNumber * secondNumber);
+}
+else if (choice === '/') {
+	alert(firstNumber / secondNumber);
+}
