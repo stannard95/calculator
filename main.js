20f600f40742f7f26ca69c116e375cfe08f53e1b
stannard95 <14066588@brookes.ac.uk>
2017-09-10 13:57:16 +0100

    added bonus tasks and invalid option choice

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-var choice = prompt('What operator would you like to do?');
+var choice = prompt('What operator would you like to do?\n\
+	[a] for addition\n\
+	[s] for subtraction\n\
+	[m] for multiplication\n\
+	[] for division\n\
+	[r] to square a number\n\
+	[p] to power your number by another number');
 
-var firstNumber = prompt('What is your first number?');
-firstNumber = parseFloat(firstNumber);
-var secondNumber = prompt('What is your second number?');
-secondNumber = parseFloat(secondNumber);
+var firstNumber = parseFloat(prompt('What is your first number?'));
+
+if (choice !== 'root') {
+	var secondNumber = parseFloat(prompt('What is your second number?'));
+
+}
 
 if (choice === '+') {
 	alert(firstNumber + secondNumber);
+
 } else if (choice === '-') {
 	alert(firstNumber - secondNumber);
-}
-else if (choice === '*') {
-	alert(firstNumber * secondNumber);
-}
-else if (choice === '/') {
-	alert(firstNumber / secondNumber);
-}
 
-else  {
+} else if (choice === '*') {
+	alert(firstNumber * secondNumber);
+
+} else if (choice === '/') {
+	alert(firstNumber / secondNumber);
+
+} else if (choice === 'power') {
+	alert(firstNumber ** secondNumber);
+
+} else if (choice === 'root') {
+	alert(Math.sqrt(firstNumber));
+
+} else  {
 	alert('Invalid choice, refresh the page to try again.');
 }
